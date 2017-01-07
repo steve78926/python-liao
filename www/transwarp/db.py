33d3fe750c37995529588894f1eceda785dbe45b
songjh <280864129@qq.com>
2017-01-07 14:04:18 +0800

    resolve decorator ident error so can't call decorator function

--- www/transwarp/db.py
+++ www/transwarp/db.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
-
+__author__ = 'Michael Liao'
 
 '''
 Database operation module.
@@ -9,11 +9,36 @@ Database operation module.
 
 import time, uuid, functools, threading, logging
 
-#Dict object:
+# Dict object:
 
 class Dict(dict):
     '''
-    simple dict but support access as x.y style.
+    Simple dict but support access as x.y style.
+    >>> d1 = Dict()
+    >>> d1['x'] = 100
+    >>> d1.x
+    100
+    >>> d1.y = 200
+    >>> d1['y']
+    200
+    >>> d2 = Dict(a=1, b=2, c='3')
+    >>> d2.c
+    '3'
+    >>> d2['empty']
+    Traceback (most recent call last):
+        ...
+    KeyError: 'empty'
+    >>> d2.empty
+    Traceback (most recent call last):
+        ...
+    AttributeError: 'Dict' object has no attribute 'empty'
+    >>> d3 = Dict(('a', 'b', 'c'), (1, 2, 3))
+    >>> d3.a
+    1
+    >>> d3.b
+    2
+    >>> d3.c
+    3
     '''
     def __init__(self, names=(), values=(), **kw):
         super(Dict, self).__init__(**kw)
@@ -32,15 +57,14 @@ class Dict(dict):
 def next_id(t=None):
     '''
     Return next id as 50-char string.
-
     Args:
-        t: unix timestamp, default to None and using time.time()
+        t: unix timestamp, default to None and using time.time().
     '''
     if t is None:
         t = time.time()
     return '%015d%s000' % (int(t * 1000), uuid.uuid4().hex)
 
-def _profiling(start, sql=''):             #error: __   --> _
+def _profiling(start, sql=''):
     t = time.time() - start
     if t > 0.1:
         logging.warning('[PROFILING] [DB] %s: %s' % (t, sql))
@@ -100,14 +124,14 @@ class _DbCtx(threading.local):
 
     def cursor(self):
         '''
-        return cursor
+        Return cursor
         '''
         return self.connection.cursor()
 
 # thread-local db context:
 _db_ctx = _DbCtx()
 
-#global engine object
+# global engine object:
 engine = None
 
 class _Engine(object):
@@ -125,19 +149,18 @@ def create_engine(user, password, database, host='127.0.0.1', port=3306, **kw):
         raise DBError('Engine is already initialized.')
     params = dict(user=user, password=password, database=database, host=host, port=port)
     defaults = dict(use_unicode=True, charset='utf8', collation='utf8_general_ci', autocommit=False)
-    for k, v in defaults.iteritems():           #error: k , ----> k,
+    for k, v in defaults.iteritems():
         params[k] = kw.pop(k, v)
     params.update(kw)
     params['buffered'] = True
     engine = _Engine(lambda: mysql.connector.connect(**params))
-    #test connection...
+    # test connection...
     logging.info('Init mysql engine <%s> ok.' % hex(id(engine)))
 
 class _ConnectionCtx(object):
     '''
     _ConnectionCtx object that can open and close connection context. _ConnectionCtx object can be nested and only the most
     outer connection has effect.
-
     with connection():
         pass
         with connection():
@@ -148,7 +171,7 @@ class _ConnectionCtx(object):
         self.should_cleanup = False
         if not _db_ctx.is_init():
             _db_ctx.init()
-            self.should_cleanup = True              #error: shound ----> should
+            self.should_cleanup = True
         return self
 
     def __exit__(self, exctype, excvalue, traceback):
@@ -159,7 +182,6 @@ class _ConnectionCtx(object):
 def connection():
     '''
     Return _ConnectionCtx object that can be used by 'with' statement:
-
     with connection():
         pass
     '''
@@ -167,17 +189,22 @@ def connection():
 
 def with_connection(func):
     '''
+    Decorator for reuse connection.
+    @with_connection
+    def foo(*args, **kw):
+        f1()
+        f2()
+        f3()
     '''
     @functools.wraps(func)
     def _wrapper(*args, **kw):
         with _ConnectionCtx():
             return func(*args, **kw)
-    return _wrapper                     #error: return 缩进不正常
+    return _wrapper
 
 class _TransactionCtx(object):
     '''
     _TransactionCtx object that can handle transactions.
-
     with _TransactionCtx():
         pass
     '''
@@ -186,11 +213,11 @@ class _TransactionCtx(object):
         global _db_ctx
         self.should_close_conn = False
         if not _db_ctx.is_init():
-            #needs open a connection first:
+            # needs open a connection first:
             _db_ctx.init()
-            self.should_close_conn = True               #error: shound  ----> should
+            self.should_close_conn = True
         _db_ctx.transactions = _db_ctx.transactions + 1
-        logging.info('begin transaction...' if _db_ctx.transactions ==1 else 'join current transaction...')
+        logging.info('begin transaction...' if _db_ctx.transactions==1 else 'join current transaction...')
         return self
 
     def __exit__(self, exctype, excvalue, traceback):
@@ -227,17 +254,47 @@ class _TransactionCtx(object):
 def transaction():
     '''
     Create a transaction object so can use with statement:
-
     with transaction():
         pass
-
+    >>> def update_profile(id, name, rollback):
+    ...     u = dict(id=id, name=name, email='%s@test.org' % name, passwd=name, last_modified=time.time())
+    ...     insert('user', **u)
+    ...     r = update('update user set passwd=? where id=?', name.upper(), id)
+    ...     if rollback:
+    ...         raise StandardError('will cause rollback...')
+    >>> with transaction():
+    ...     update_profile(900301, 'Python', False)
+    >>> select_one('select * from user where id=?', 900301).name
+    u'Python'
+    >>> with transaction():
+    ...     update_profile(900302, 'Ruby', True)
+    Traceback (most recent call last):
+      ...
+    StandardError: will cause rollback...
+    >>> select('select * from user where id=?', 900302)
+    []
     '''
     return _TransactionCtx()
 
 def with_transaction(func):
     '''
     A decorator that makes function around transaction.
-
+    >>> @with_transaction
+    ... def update_profile(id, name, rollback):
+    ...     u = dict(id=id, name=name, email='%s@test.org' % name, passwd=name, last_modified=time.time())
+    ...     insert('user', **u)
+    ...     r = update('update user set passwd=? where id=?', name.upper(), id)
+    ...     if rollback:
+    ...         raise StandardError('will cause rollback...')
+    >>> update_profile(8080, 'Julia', False)
+    >>> select_one('select * from user where id=?', 8080).passwd
+    u'JULIA'
+    >>> update_profile(9090, 'Robert', True)
+    Traceback (most recent call last):
+      ...
+    StandardError: will cause rollback...
+    >>> select('select * from user where id=?', 9090)
+    []
     '''
     @functools.wraps(func)
     def _wrapper(*args, **kw):
@@ -245,7 +302,7 @@ def with_transaction(func):
         with _TransactionCtx():
             return func(*args, **kw)
         _profiling(_start)
-    return _wrapper                    #error: return 缩进不正常
+    return _wrapper
 
 def _select(sql, first, *args):
     ' execute select SQL and return unique result or list results.'
@@ -274,7 +331,19 @@ def select_one(sql, *args):
     Execute select SQL and expected one result.
     If no result found, return None.
     If multiple results found, the first one returned.
-
+    >>> u1 = dict(id=100, name='Alice', email='alice@test.org', passwd='ABC-12345', last_modified=time.time())
+    >>> u2 = dict(id=101, name='Sarah', email='sarah@test.org', passwd='ABC-12345', last_modified=time.time())
+    >>> insert('user', **u1)
+    1
+    >>> insert('user', **u2)
+    1
+    >>> u = select_one('select * from user where id=?', 100)
+    >>> u.name
+    u'Alice'
+    >>> select_one('select * from user where email=?', 'abc@email.com')
+    >>> u2 = select_one('select * from user where passwd=? order by email', 'ABC-12345')
+    >>> u2.name
+    u'Alice'
     '''
     return _select(sql, True, *args)
 
@@ -301,7 +370,6 @@ def select_int(sql, *args):
     Traceback (most recent call last):
         ...
     MultiColumnsError: Expect only one column.
-
     '''
     d = _select(sql, True, *args)
     if len(d)!=1:
@@ -311,10 +379,10 @@ def select_int(sql, *args):
 @with_connection
 def select(sql, *args):
     '''
-    Excute select SQL and return list or empty list if no result.
+    Execute select SQL and return list or empty list if no result.
     >>> u1 = dict(id=200, name='Wall.E', email='wall.e@test.org', passwd='back-to-earth', last_modified=time.time())
     >>> u2 = dict(id=201, name='Eva', email='eva@test.org', passwd='back-to-earth', last_modified=time.time())
-    >>> insert('user', **u1)           #error: 缩进必须4个空格，否则报错
+    >>> insert('user', **u1)
     1
     >>> insert('user', **u2)
     1
@@ -354,7 +422,6 @@ def _update(sql, *args):
 def insert(table, **kw):
     '''
     Execute insert SQL.
-
     >>> u1 = dict(id=2000, name='Bob', email='bob@test.org', passwd='bobobob', last_modified=time.time())
     >>> insert('user', **u1)
     1
@@ -373,7 +440,6 @@ def insert(table, **kw):
 def update(sql, *args):
     r'''
     Execute update SQL.
-
     >>> u1 = dict(id=1000, name='Michael', email='michael@test.org', passwd='123456', last_modified=time.time())
     >>> insert('user', **u1)
     1
@@ -394,7 +460,7 @@ def update(sql, *args):
     '''
     return _update(sql, *args)
 
-if __name__ == '__main__':
+if __name__=='__main__':
     logging.basicConfig(level=logging.DEBUG)
     create_engine('www-data', 'www-data', 'test')
     update('drop table if exists user')
